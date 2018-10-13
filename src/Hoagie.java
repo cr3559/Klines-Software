96bf8620bf6904b0cb9dbaedadf3e5ad20ad5815
cr3559 <cr3559@cs.ship.edu>
2018-10-13 15:45:12 -0400

    Hoagie Constructor

---
+++ src/Hoagie.java
@@ -0,0 +1,12 @@
+
+public class Hoagie 
+{
+
+	public Hoagie(boolean mayo, String meat, String cheese, boolean onion, boolean tomatoe, boolean lettuce, boolean hots, String specialRequests)
+	{
+		
+	}
+	
+	
+	
+}
