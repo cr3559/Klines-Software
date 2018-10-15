4447bb3d7f7dc604f8d18e102c50542151fb1e49
cr3559 <cr3559@cs.ship.edu>
2018-10-15 18:59:28 -0400

    added instance vars to Hoagie class

--- src/Hoagie.java
+++ src/Hoagie.java
@@ -2,8 +2,25 @@
 public class Hoagie 
 {
 
-	public Hoagie(boolean mayo, String meat, String cheese, boolean onion, boolean tomatoe, boolean lettuce, boolean hots, String specialRequests)
+	boolean mayo;
+	String meat;
+	String cheese;
+	boolean onion;
+	boolean tomato;
+	boolean lettuce;
+	boolean hots;
+	String specialRequests;
+	
+	public Hoagie(boolean mayo, String meat, String cheese, boolean onion, boolean tomato, boolean lettuce, boolean hots, String specialRequests)
 	{
+		this.mayo = mayo;
+		this.meat = meat;
+		this.onion =onion;
+		this.tomato = tomato;
+		this.lettuce = lettuce;
+		this.hots =hots;
+		this.specialRequests = specialRequests;
+		
 		
 	}
 	
